c2db7a3ccde795428f2b74db7672ff0d39d39252
M Raian <mraian@Raians-MacBook-Pro.local>
2024-02-07 15:35:14 -0500

    first commit

---
+++ pages/homePage.js
@@ -0,0 +1,17 @@
+const { expect } = require("@playwright/test");
+
+exports.homePage = class homePage{
+
+    constructor(page){
+        this.page = page;
+        this.homeButton = page.locator("//a[text()='Home ']");
+        //contactButton
+        //logInButton
+    }
+    async go(){
+        await this.page.goto('https://demoblaze.com/index.html#');
+    }
+    async checkHomeButton(){
+        await expect(this.homeButton).toBeVisible();
+    }
+}
---
+++ tests/homePageTests.spec.js
@@ -0,0 +1,10 @@
+import{test, expect} from '@playwright/test';
+import{homePage} from '../pages/homePage';
+
+test('homePageTest', async({page})=>{
+
+    const h1 = new homePage(page);
+
+   await h1.go();
+   await h1.checkHomeButton();
+})
